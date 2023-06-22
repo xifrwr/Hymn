8b1a98de9e04649016033fd9218d2b417ebc9e61
xifrwr <66585390+xifrwr@users.noreply.github.com>
2023-06-23 02:14:04 +0800

    Add files via upload

---
+++ slash/cat.js
@@ -0,0 +1,36 @@
+﻿/*const colors = require("colors");
+const { MessageEmbed } = require("discord.js");
+const SlashCommand = require("../../lib/SlashCommand");
+const got = require('got');
+
+const command = new SlashCommand()
+    .setName("cat")
+    .setDescription("Shows random cats on the internet!")
+    .setRun(async (client, interaction) => {
+        async function cat() {
+            await fetch('https://www.reddit.com/r/cats/random/.json')
+                .then(async r => {
+                    let cat = await r.json();
+                    let catImage = cat[0].data.children[0].data.url;
+                    let catTitle = cat[0].data.children[0].data.title;
+                    let catUpvotes = cat[0].data.children[0].data.ups;
+                    let catDownvotes = cat[0].data.children[0].data.downs;
+                    let catNumComments = cat[0].data.children[0].data.num_comments;
+
+                    const embed = new MessageEmbed()
+                        .setColor('RANDOM')
+                        .setTitle(`${catTitle}`)
+                        .setImage(`${catImage}`)
+                        .setURL(`${catImage}`)
+                        .setFooter(`⬆️ ${catUpvotes} ⬇️ ${catDownvotes}`)
+
+                    return interaction.reply({ embeds: [embed] });
+
+                })
+
+        }
+        cat();
+    });
+
+module.exports = command;
+*/
